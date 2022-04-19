3440325e177ac55f8218b6d3532bb449196aaae9
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-04-19 17:14:24 -0400

    attempt to test thru envoy

--- etc/testing/circle/kube_debug.sh
+++ etc/testing/circle/kube_debug.sh
@@ -30,6 +30,7 @@ cmds=(
   # Set --tail b/c by default 'kubectl logs' only outputs 10 lines if -l is set
   'kubectl logs --tail=1500 -l suite=pachyderm,app=pachd'
   'kubectl logs --tail=1500 -l suite=pachyderm,app=pachd --previous # if pachd restarted'
+  'kubectl logs --tail=1500 -l suite=pachdyerm,app=pachyderm-proxy'
   'sudo dmesg | tail -n 40'
   'minikube logs | tail -n 100'
   'top -b -n 1 | head -n 40'
